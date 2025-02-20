943c1e6c79508c59ac05e50694ba2792bac75b53
Ryan Zhu <ryanzhu@berkeley.edu>
2025-02-19 17:51:09 -0800

    Fix C++ solution

--- unlockmanifolds/submissions/accepted/unlockmanifolds.cpp
+++ unlockmanifolds/submissions/accepted/unlockmanifolds.cpp
@@ -7,22 +7,22 @@ int solve(int N, int M, vector<vector<int>> &G) {
     vector<int> row(N * M + 1), col(N * M + 1);
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            row[G[i][j]] = j;
-            col[G[i][j]] = i;
+            row[G[i][j]] = i;
+            col[G[i][j]] = j;
         }
     }
     
     int actions = 0;
-    for (int i = 1; i <= N * M; i++) {
+    for (int i = 2; i <= N * M; i++) {
         // vertical move actions
         actions += min((row[i] - row[i - 1] + N) % N, (row[i - 1] - row[i] + N) % N);
         // horizontal move actions
         actions += min((col[i] - col[i - 1] + M) % M, (col[i - 1] - col[i] + M) % M);
         // note: % in C++ can return a negative value, so we need to add N or M to normalize it
-        
-        // click actions
-        actions += 1;
     }
+    
+    // click actions
+    actions += N * M;
     
     return actions;
 }
