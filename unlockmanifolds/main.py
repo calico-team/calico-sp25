105efde8bbb1ac365990eb5ccd6f21b8af176190
Matthew <matthewslee1369@berkeley.edu>
2025-02-27 19:16:14 -0800

    testing test cases

--- unlockmanifolds/main.py
+++ unlockmanifolds/main.py
@@ -12,17 +12,17 @@ from typing import NamedTuple, override
 import random
 
 p = Problem["TestFile"](
-        'add',
+        'unlockmanifolds',
         test_sets=[
-            Subproblem('main', rank=1),
-            Subproblem('bonus', rank=2, time_limit=4, mem_limit=1_000_000_000),
+            Subproblem('main', rank=2),
             ])
 
 class TestCase(NamedTuple):
-    X: int
-    Y: int
+    N: int
+    M: int
+    G: list
 
-solution = py_runner('submissions/accepted/unlockmanifolds_matt.py')
+solution = py_runner('unlockmanifolds/submissions/accepted/unlockmanifolds.py')
 # validator1 = py_runner('scripts/validator_main.py')
 # validator2 = py_runner('scripts/validator.py')
 
@@ -36,7 +36,12 @@ class TestFile(TestFileBase):
         """Write the input file of this test case using print_test"""
         p.print_test(len(self.cases))
         for case in self.cases:
-            p.print_test(case.X, case.Y)
+            p.print_test(case.N, case.M)
+            for line in case.G:
+                temp = ""
+                for num in line:
+                    temp = temp + " " + str(num)
+                p.print_test(temp.strip())
 
     @override
     def validate_test_in(self, infile: str):
@@ -47,41 +52,60 @@ class TestFile(TestFileBase):
 
     @override
     def write_test_out(self, infile: str):
-        pass
-        # p.print_test(solution.exec_file(infile))
+        p.print_test(solution.exec_file(infile))
 
 # adds to all subproblems by default
 p.add_sample_test(TestFile([
-    TestCase(4, 7),
-    TestCase(1, 23),
-    TestCase(9, 8),
-    TestCase(1, 1),
+    TestCase(3, 3, [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]),
+    TestCase(4, 2, [
+        [1, 3],
+        [5, 7],
+        [2, 4],
+        [6, 8]
+    ]),
     ]))
 
 cases = []
-for i in range(80):
-    cases.append(TestCase(i+1, 80-i))
+for i in range(10):
+    N = random.randint(1, 200)
+    M = random.randint(1, 200)
+    numbers = list(range(1, N * M))
+    random.shuffle(numbers)
+    G = []
+    temp = []
+    count = 0
+    for num in numbers:
+        temp.append(num)
+        if (count == M):
+            G.append(temp)
+            temp = []
+            count = 0
+    cases.append(TestCase(N, M, G))
 
-p.add_hidden_test(TestFile(cases), 'iota')
+p.add_hidden_test(TestFile(cases), 'secret_01_main_random.in')
     
-cases = []
-for i in range(100):
-    cases.append(TestCase(i+1, 10000-i))
+#cases = []
+#for i in range(100):
+#    cases.append(TestCase(i+1, 10000-i))
 
-p.add_hidden_test(TestFile(cases), 'iota', subproblems=['bonus'])
+#p.add_hidden_test(TestFile(cases), 'iota', subproblems=['bonus'])
 
 # more ways to add test cases
-@p.hidden_test_generator(test_count=4)
-def pure_random() -> TestFile:
-    test = TestFile([])
-    for i in range(10):
-        test.cases.append(TestCase(random.randint(1, 100), random.randint(1, 100)))
-    return test
+#@p.hidden_test_generator(test_count=4)
+#def pure_random() -> TestFile:
+#    test = TestFile([])
+#    for i in range(10):
+#        test.cases.append(TestCase(random.randint(1, 100), random.randint(1, 100)))
+#    return test
 
-@p.hidden_test_generator(test_count=4, subproblems=['bonus'])
-def pure_random2():
-    cases = (TestCase(random.randint(70, int(1e12)), random.randint(70, int(1e12))) for _ in range(100))
-    return TestFile(cases)
+#@p.hidden_test_generator(test_count=4, subproblems=['bonus'])
+#def pure_random2():
+#    cases = (TestCase(random.randint(70, int(1e12)), random.randint(70, int(1e12))) for _ in range(100))
+#    return TestFile(cases)
 
 def main():
     # p.run_cli()
