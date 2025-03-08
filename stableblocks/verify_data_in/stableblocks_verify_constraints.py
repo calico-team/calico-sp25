c6f9308a62a41c0a779a41e1cca1fa6b181f3e34
Matthias Goerner <1239022+unhyperbolic@users.noreply.github.com>
2025-03-08 10:42:05 -0800

    Adding code to verify constraints for input files.

---
+++ stableblocks/verify_data_in/stableblocks_verify_constraints.py
@@ -0,0 +1,55 @@
+import re
+
+pos_int_re = re.compile(r'^[1-9]\d*$')
+int_pattern = r'(0|-?[1-9]\d*)'
+block_re = re.compile(r'^' + ' '.join(4 * [int_pattern]) + '$')
+
+if __name__ == '__main__':
+    import sys
+
+    lines = open(sys.argv[1]).read().split('\n')
+
+    line_no = 0
+
+    m = pos_int_re.match(lines[line_no])
+    if not m:
+        raise Exception("Bad line %d" % line_no)
+    line_no += 1
+    T = int(m.group(0))
+
+    for i in range(T):
+        m = pos_int_re.match(lines[line_no])
+        if not m:
+            raise Exception("Bad line %d" % line_no)
+        line_no += 1
+        N = int(m.group(0))
+
+        total_mass = 0
+
+        for j in range(N):
+            m = block_re.match(lines[line_no])
+            if not m:
+                raise Exception("Bad line %s %d" % (lines[line_no], line_no))
+            line_no += 1
+            x0, y0, x1, y1 = [int(s) for s in m.groups()]
+            if not x1 > x0:
+                raise Exception("Not x1 > x0")
+            if not y1 > y0:
+                raise Exception("Not x1 > x0")
+            if not y0 >= 0:
+                raise Exception("Not y0 >= 0")
+            if not x0 > -10000:
+                raise Exception("Not x0 > -10000")
+            if not x0 < 10000:
+                raise Exception("Not x0 < 10000")
+            total_mass += (x1 - x0) * (y1 - y0)
+
+        if not total_mass < 100000:
+            raise Exception("Not total_mass < 100000")
+
+    if lines[line_no] != '':
+        raise Exception("Not ending with new line")
+    line_no += 1
+
+    if line_no != len(lines):
+        raise Exception("Wrong number lines %d %d" % (line_no, len(lines)))
---
+++ stableblocks/verify_data_in/stableblocks_verify_forest.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+
+struct Corner
+{
+    int x, y;
+};
+
+struct Block
+{
+    Corner lowerLeft;
+    Corner upperRight;
+};
+
+bool is_forest(const std::vector<Block> &blocks)
+{
+    std::map<std::pair<int, int>, int> pointToBlockIndex;
+    std::vector<std::set<int>> blocksToBlocksItIsRestingOn(blocks.size());
+    std::vector<std::set<int>> blocksToBlocksRestingOnIt(blocks.size());
+
+    for (int i = 0; i < blocks.size(); ++i) {
+	for (int x = blocks[i].lowerLeft.x; x < blocks[i].upperRight.x; ++x) {
+	    for (int y = blocks[i].lowerLeft.y; y < blocks[i].upperRight.y; ++y) {
+		if (!pointToBlockIndex.insert({{x,y}, i}).second) {
+		    return false;
+		}
+	    }
+	}
+    }
+
+    for (int i = 0; i < blocks.size(); ++i) {
+	for (int y = blocks[i].lowerLeft.y; y < blocks[i].upperRight.y; ++y) {
+	    if (pointToBlockIndex.find({blocks[i].lowerLeft.x - 1, y}) != pointToBlockIndex.end()) {
+		return false;
+	    }
+	    if (pointToBlockIndex.find({blocks[i].upperRight.x, y}) != pointToBlockIndex.end()) {
+		return false;
+	    }
+	}
+	for (int x = blocks[i].lowerLeft.x; x < blocks[i].upperRight.x; ++x) {
+	    {
+		const auto it = pointToBlockIndex.find({x, blocks[i].lowerLeft.y - 1});
+		if (it != pointToBlockIndex.end()) {
+		    blocksToBlocksItIsRestingOn[i].insert(it->second);
+		}
+	    }
+	    {
+		const auto it = pointToBlockIndex.find({x, blocks[i].upperRight.y});
+		if (it != pointToBlockIndex.end()) {
+		    blocksToBlocksRestingOnIt[i].insert(it->second);
+		}
+	    }
+	}
+    }
+    
+    for (int i = 0; i < blocks.size(); i++) {
+	const int n = blocksToBlocksItIsRestingOn[i].size();
+	if (n == 0) {
+	    if (blocks[i].lowerLeft.y != 0) {
+		return false;
+	    }
+	}
+	if (n > 1) {
+	    return false;
+	}
+    }
+    
+    return true;
+}
+
+int main()
+{
+    int numCases;
+    std::cin >> numCases;
+
+    for (int i = 0; i < numCases; i++) {
+	int numBlocks;
+	std::cin >> numBlocks;
+
+	std::vector<Block> blocks;
+	blocks.reserve(numBlocks);
+
+	for (int b = 0; b < numBlocks; b++) {
+	    Block block;
+	    std::cin >> block.lowerLeft.x >> block.lowerLeft.y
+		     >> block.upperRight.x >> block.upperRight.y;
+	    blocks.push_back(std::move(block));
+	}
+
+	if (!is_forest(blocks)) {
+	    std::cout << "NOT A FORREST" << std::endl;
+	    return 1;
+	}
+    }
+    return 0;
+}
