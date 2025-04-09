436057796762f136e08e52f818410e97ac2d5551
Connor Chang <connorc0627@gmail.com>
2025-04-09 16:57:18 -0700

    new calico lib; lock file

--- unlockmanifolds/main.py
+++ unlockmanifolds/main.py
@@ -121,9 +121,9 @@ p.add_hidden_test(TestFile(cases), 'secret_01_main_random')
 #    return TestFile(cases)
 
 def main():
-    # p.run_cli()
-    p.create_all_tests()
-    p.create_zip()
+    p.run_cli()
+    # p.create_all_tests()
+    # p.create_zip()
 
 if __name__ == '__main__':
     main()
