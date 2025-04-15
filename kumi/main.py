0e8c450c0458ca1ec6f1f44a7479dcc4b4db1cbd
Connor Chang <connorc0627@gmail.com>
2025-04-15 14:05:51 -0700

    wip kumi

---
+++ kumi/main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# Example add problem.
+# Constraints:
+#   main: T <= 100, A <= 100, B <= 100
+#   bonus: T <= 1e5, A <= 1e12, B <= 1e12
+
+from typing import override
+from calico_lib import Problem, cpp_runner, py_runner, TestFileBase, MulticaseTestFile, Subproblem, Runner
+from collections.abc import Collection, Iterable
+from typing import NamedTuple, override
+import random
+import os
+from os import path
+
+from calico_lib.multicase import TestCaseBase
+
+problem_dir = os.path.dirname(__file__)
+
+p = Problem(
+        'kumi',
+        problem_dir, # problem is in the same directory as the python source file
+        test_sets=[
+            Subproblem('main', rank=1),
+            ])
+
+solution = py_runner(path.join(problem_dir, 'submissions/accepted/sol.py'))
+
+class TestFile(TestFileBase):
+    def __init__(self, cases: Iterable[int]) -> None:
+        self.cases = list(cases)
+        super().__init__()
+
+    @override
+    def write_test_in(self):
+        """Write the input file of this test case using print_test"""
+        p.print_test(len(self.cases))
+        for case in self.cases:
+            p.print_test(case)
+
+    @override
+    def validate_test_in(self, infile: str):
+        """Verify the test using an external validator."""
+        # validator1.exec_file(infile)
+
+    @override
+    def write_test_out(self, infile: str):
+        p.print_test(solution.exec_file(infile))
+
+# adds to all subproblems by default
+p.add_sample_test(TestFile([5, 100, 20, 30]))
+
+# more ways to add test cases
+# @p.hidden_test_generator(test_count=4)
+# def pure_random() -> TestFile:
+#     test = TestFile([])
+#     for i in range(10):
+#         test.cases.append(TestCase(random.randint(1, 100), random.randint(1, 100)))
+#     return test
+
+def main():
+    # increase stack size for running solutions using heaving recursion
+    # import resource
+    # resource.setrlimit(resource.RLIMIT_STACK, (268435456, 268435456))
+
+    # TODO: set seed
+    random.seed('hiding_in_your_wifi')
+    solution.compile()
+    p.init_problem()
+    p.run_cli()
+
+    # p.create_all_tests()
+    # p.create_zip()
+
+if __name__ == '__main__':
+    main()
---
+++ kumi/submissions/accepted/sol.py
@@ -0,0 +1,43 @@
+"""
+solution sketch:
+    start with uwuwuwuwuwuwuw = "uw"*m
+    answer is m*(m-1)//2
+
+    remove 3 consecutive w's (uwuwuwuw ==> uuuuw): answer reduces by 6
+    remove a single w (uwu ==> uu): answer reduces by 1
+"""
+N = int(input())
+
+m = 2
+while m*m - m < 2*N:
+    m += 1
+
+diff = (m*m-m) - 2*N
+
+d = diff // 12
+# print(f"m = {m}")
+# print(f"diff = {diff}")
+# print(f"d = {d}")
+# print(d*12)
+diff -= d*12
+b = diff // 2
+# print(b)
+
+a = m-d*4-b*2
+# print(f"{d} 4s; {b} 2s; {a} 1s;")
+print("uw"*a + "uuw"*b + "uuuuw"*d)
+
+# arr = [1]*a + [2]*b + [4]*d
+# total = 0
+
+## use formula to verify
+# total = a*(a-1)//2 + 4*b*(b-1)//2 + 16*d*(d-1)//2 + a*b*2 + a*d*4 + b*d*8
+
+# print(f"using formula,\n{total} should equal N: {N}")
+
+## use double for loop to verify
+# for i in range(len(arr)):
+#     for j in range(i+1, len(arr)):
+#         total += arr[i]*arr[j]
+#
+# print(f"{total} should equal N: {N}")
