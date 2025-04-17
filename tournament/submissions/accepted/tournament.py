4c5799e85d7407ff7502c54d64d74107cba3ce1e
Matthew <matthewslee1369@berkeley.edu>
2025-04-16 17:37:16 -0700

    tournament java and python sols

--- tournament/submissions/accepted/tournament.py
+++ tournament/submissions/accepted/tournament.py
@@ -1,4 +1,11 @@
-def solve(C, P):
+def solve(N: int, C: list, P: list) -> str:
+    """
+    Return a single string of the champion's name
+    
+    N: The length of C and P
+    C: List of strings of the competitors
+    P: List of integers of competitor's power
+    """
     def helper(competitors, powers):
         if (len(competitors) == 1):
             return competitors[0]
@@ -13,11 +20,14 @@ def solve(C, P):
         return helper(winners, winnerPowers)
     return helper(C, P)
 
+
 def main():
-    T = input()
-    for _ in range(int(T)):
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
         C = input().split(" ")
         P = list(map(lambda s: int(s), input().split(" ")))
-        print(solve(C, P))
+        print(solve(N, C, P))
 
-main()
+if __name__ == '__main__':
+    main()
