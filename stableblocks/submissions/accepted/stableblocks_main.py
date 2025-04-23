9ac453c5cde0022e94a98642773c515780179781
Ryan Zhu <ryanzhu@berkeley.edu>
2025-04-22 17:46:07 -0700

    Implement python template and sol

---
+++ stableblocks/submissions/accepted/stableblocks_main.py
@@ -0,0 +1,90 @@
+from bisect import bisect
+from functools import cache
+
+def solve(N: int, X0: list[int], Y0: list[int], X1: list[int], Y1: list[int]) -> bool:
+    # group blocks together by their vertical position
+    groups = {}
+    for i in range(N):
+        if Y0[i] not in groups:
+            groups[Y0[i]] = []
+        groups[Y0[i]].append((X0[i], i))
+    
+    # sort blocks within each group by their horizontal position
+    for bottoms in groups.values():
+        bottoms.sort()
+    
+    @cache
+    def blocks_resting_on_block(i):
+        nonlocal groups
+        
+        if Y1[i] not in groups:
+            return []
+        candidates = groups[Y1[i]]
+        
+        resting_blocks = []
+        for index in range(bisect(candidates, (X1[i], -1)) - 1, -1, -1):
+            _, j = candidates[index]
+            if X1[j] < X0[i]:
+                break
+            resting_blocks.append(j)
+        
+        return resting_blocks
+    
+    @cache
+    def mass_of_block(i):
+        return (X1[i] - X0[i]) * (Y1[i] - Y0[i])
+    
+    @cache
+    def relevant_mass_of_block(i):
+        mass = mass_of_block(i)
+        for j in blocks_resting_on_block(i):
+            mass += relevant_mass_of_block(j)
+        return mass
+    
+    @cache
+    def center_of_mass_of_block(i):
+        return (X0[i] + X1[i]) / 2, (Y0[i] + Y1[i]) / 2
+    
+    @cache
+    def relevant_center_of_mass_of_block(i):
+        x_center_i, y_center_i = center_of_mass_of_block(i)
+        mass_i = mass_of_block(i)
+        
+        x_centers, y_centers, masses = [x_center_i], [y_center_i], [mass_i]
+        for j in blocks_resting_on_block(i):
+            x_center_j, y_center_j = relevant_center_of_mass_of_block(j)
+            mass_j = relevant_mass_of_block(j)
+            x_centers.append(x_center_j)
+            y_centers.append(y_center_j)
+            masses.append(mass_j)
+        
+        x = sum(x * m for x, m in zip(x_centers, masses)) / sum(masses)
+        y = sum(y * m for y, m in zip(x_centers, masses)) / sum(masses)
+        
+        return x, y
+        
+    for i in range(N):
+        for j in blocks_resting_on_block(i):
+            x, y = relevant_center_of_mass_of_block(j)
+            if not X0[i] < x < X1[i]:
+                return False
+    
+    return True
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        corners = []
+        for _ in range(N):
+            corners.append(list(map(int, input().split())))
+        X0, Y0, X1, Y1 = zip(*corners)
+        if solve(N, X0, Y0, X1, Y1):
+            print('Stable')
+        else:
+            print('Unstable')
+
+
+if __name__ == '__main__':
+    main()
--- stableblocks/templates/stableblocks_template.py
+++ stableblocks/templates/stableblocks_template.py
@@ -1,21 +1,30 @@
-def is_stable(blocks : list[tuple[tuple[int, int], tuple[int, int]]]) -> bool:
+def solve(N: int, X0: list[int], Y0: list[int], X1: list[int], Y1: list[int]) -> bool:
+    """
+    Return True if the configuration of blocks is stable, and False otherwise.
+    
+    N: the number of blocks
+    X0: a list containing the X-coordinates of the lower left corner of each block
+    Y0: a list containing the Y-coordinates of the lower left corner of each block
+    X1: a list containing the X-coordinates of the upper right corner of each block
+    Y1: a list containing the Y-coordinates of the upper right corner of each block
+    """
     # YOUR CODE HERE
-    return True
+    return False
+
 
 def main():
     T = int(input())
-    for j in range(T):
+    for _ in range(T):
         N = int(input())
-        blocks = []
-        for i in range(N):
-            lowerLeftX, lowerLeftY, upperRightX, upperRightY = input().split()
-            blocks.append(
-                ((int(lowerLeftX),  int(lowerLeftY)),
-                 (int(upperRightX), int(upperRightY))))
-        if is_stable(blocks):
-            print("Stable")
+        corners = []
+        for _ in range(N):
+            corners.append(list(map(int, input().split())))
+        X0, Y0, X1, Y1 = zip(*corners)
+        if solve(N, X0, Y0, X1, Y1):
+            print('Stable')
         else:
-            print("Unstable")
+            print('Unstable')
+
 
 if __name__ == '__main__':
     main()
