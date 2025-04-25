260bc66927bed76b7708c27b56a1a45ce432d2cc
Connor Chang <connorc0627@gmail.com>
2025-04-25 16:14:29 -0700

    [auto generated by seed_script.sh]: seed stuff

--- example/main.py
+++ example/main.py
@@ -16,6 +16,7 @@ from calico_lib.multicase import TestCaseBase
 
 problem_dir = os.path.dirname(__file__)
 
+random.seed('add_seed_600')
 p = Problem(
         'add',
         problem_dir, # problem is in the same directory as the python source file
@@ -97,7 +98,6 @@ def main():
     # resource.setrlimit(resource.RLIMIT_STACK, (268435456, 268435456))
 
     # TODO: set seed
-    random.seed('add_seed_600')
     solution2.compile()
     p.init_problem()
     p.create_all_tests()
