dd4676d1b175d78346d8a69423658eb95125cd03
mantlemoose <alexdu9876@gmail.com>
2025-04-25 21:04:07 -0700

    Finished and added to testing

--- gates/main.py
+++ gates/main.py
@@ -17,23 +17,23 @@ from calico_lib.multicase import TestCaseBase
 
 problem_dir = os.path.dirname(__file__)
 
-random.seed('add_seed_600')
+random.seed('123456')
 p = Problem(
-        'add',
+        'gates',
         problem_dir, # problem is in the same directory as the python source file
         test_sets=[
-            Subproblem('main', rank=1),
-            Subproblem('bonus', rank=2, time_limit=4, mem_limit=1_000_000_000),
+            Subproblem('main', rank=3),
+            Subproblem('bonus', rank=3),
             ])
 
 class TestCase(NamedTuple):
-    X: int
-    Y: int
+    N: int
+    lst: list 
 
-solution = py_runner(path.join(problem_dir, 'submissions/accepted/add_arbitrary.py'))
-solution2 = cpp_runner(
-        path.join(problem_dir, 'submissions/accepted/add_int.cpp'),
-        path.join(problem_dir, 'add_int.bin'))
+solution = py_runner(
+        path.join(problem_dir, "submissions/accepted/solution.py"))
+solution2 = py_runner(
+        path.join(problem_dir, 'submissions/time_limit_exceeded/brute_force.py'))
 validator1 = py_runner(path.join(problem_dir, 'scripts/validator_main.py'))
 validator2 = py_runner(path.join(problem_dir, 'scripts/validator.py'))
 
@@ -47,7 +47,8 @@ class TestFile(TestFileBase):
         """Write the input file of this test case using print_test"""
         p.print_test(len(self.cases))
         for case in self.cases:
-            p.print_test(case.X, case.Y)
+            p.print_test(case.N)
+            p.print_test(' '.join(str(x) for x in case.lst))
 
     @override
     def validate_test_in(self, infile: str):
@@ -58,40 +59,45 @@ class TestFile(TestFileBase):
 
     @override
     def write_test_out(self, infile: str):
-        p.print_test(solution2.exec_file(infile))
+        p.print_test(solution.exec_file(infile))
 
 # adds to all subproblems by default
 p.add_sample_test(TestFile([
-    TestCase(4, 7),
-    TestCase(1, 23),
-    TestCase(9, 8),
-    TestCase(1, 1),
+    TestCase(2, [1, 1]),
+    TestCase(3, [1, 0, 1]),
     ]))
 
 cases = []
-for i in range(80):
-    cases.append(TestCase(i+1, 80-i))
+for i in range(10):
+    lst = []
+    for j in range(5):
+        lst.append(random.randint(0, 1))
+    cases.append(TestCase(5, lst))
 
-p.add_hidden_test(TestFile(cases), 'iota')
+p.add_hidden_test(TestFile(cases), 'small')
     
-cases = []
-for i in range(100):
-    cases.append(TestCase(i+1, 10000-i))
-
-p.add_hidden_test(TestFile(cases), 'iota', subproblems=['bonus'])
-
 # more ways to add test cases
 @p.hidden_test_generator(test_count=4)
 def pure_random() -> TestFile:
     test = TestFile([])
-    for i in range(10):
-        test.cases.append(TestCase(random.randint(1, 100), random.randint(1, 100)))
+    for i in range(2):
+        n = random.randint(400, 500)
+        lst = []
+        for j in range(n):
+            lst.append(random.randint(0, 1))
+        test.cases.append(TestCase(n, lst))
     return test
 
 @p.hidden_test_generator(test_count=4, subproblems=['bonus'])
 def pure_random2():
-    cases = (TestCase(random.randint(70, int(1e12)), random.randint(70, int(1e12))) for _ in range(100))
-    return TestFile(cases)
+    test = TestFile([])
+    for i in range(2):
+        n = random.randint(80000, 100000)
+        lst = []
+        for j in range(n):
+            lst.append(random.randint(0, 1))
+        test.cases.append(TestCase(n, lst))
+    return test
 
 def main():
     # increase stack size for running solutions using heaving recursion
@@ -99,7 +105,6 @@ def main():
     # resource.setrlimit(resource.RLIMIT_STACK, (268435456, 268435456))
 
     # TODO: set seed
-    solution2.compile()
     p.run_cli()
 
     # p.init_problem()
